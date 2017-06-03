b9af35f5b12f0bed4a2f6f8e69248dbc91acf7ef
Dima <sumskoydima@gmail.com>
2017-06-03 23:28:37 +0300

    Added assignment and uniqueness of railcar number

--- tutu/app/models/railcar.rb
+++ tutu/app/models/railcar.rb
@@ -1,5 +1,17 @@
 class Railcar < ApplicationRecord
   belongs_to :train
 
-  validates :number, presence: true
+  validates :number, presence: true, uniqueness: { scope: :train_id }
+
+  before_validation :set_number
+
+  private
+
+  def set_number
+    if self.train.railcars.size.zero?
+      self.number = 1
+    else
+      self.number = self.train.railcars.last.number + 1
+    end
+  end
 end
