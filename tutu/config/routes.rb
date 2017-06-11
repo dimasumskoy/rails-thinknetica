6ec5f6ba26a64b7def35be487a379e41695d3579
Dima <sumskoydima@gmail.com>
2017-06-11 21:01:43 +0300

    Added time form to route 'show' view

--- tutu/config/routes.rb
+++ tutu/config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   end
   resources :railway_stations do
     patch :update_position, on: :member
+    patch :set_arrival_time, on: :member
+    patch :set_departure_time, on: :member
   end
   resources :routes
 
