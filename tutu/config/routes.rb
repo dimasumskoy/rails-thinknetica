9e3cff2c03bd699ae162a4da9c2bce0392d46901
Dima <sumskoydima@gmail.com>
2017-06-11 23:27:07 +0300

    Added 'search' init files

--- tutu/config/routes.rb
+++ tutu/config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
     patch :set_time, on: :member
   end
   resources :routes
+  resource :search, only: [:new, :show, :edit]
 
   get 'welcome/index'
-
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
