5afdf94eab8b60d7cbcc03f5d1e97e908cd40058
Dima <sumskoydima@gmail.com>
2017-06-19 10:41:29 +0300

    Refactored

--- tutu/app/controllers/application_controller.rb
+++ tutu/app/controllers/application_controller.rb
@@ -3,8 +3,10 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource_or_scope)
     if resource_or_scope.admin?
+      flash[:notice] = "Hi, admin"
       admin_root_path
     else
+      flash[:notice] = "Hi, #{current_user.first_name}"
       search_path
     end
   end
