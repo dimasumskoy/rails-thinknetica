eb5c2c674e4ea6e54d3748e07d43bea168014a6b
Dima <sumskoydima@gmail.com>
2017-06-29 12:08:41 +0300

    Refactored js for search

--- tutu/app/assets/javascripts/search.js
+++ tutu/app/assets/javascripts/search.js
@@ -1,9 +1,9 @@
 $(document).on('turbolinks:load', function() {
     $('.search-form').submit(function() {
-        var depart_station = $(this).find('#stations_first_station_id')
-        var arrive_station = $(this).find('#stations_last_station_id')
+        var departStation = $(this).find('#stations_first_station_id')
+        var arriveStation = $(this).find('#stations_last_station_id')
 
-        if (depart_station.val() == arrive_station.val()) {
+        if (departStation.val() == arriveStation.val()) {
             alert('Выберите конечную станцию');
             return false;
         }
